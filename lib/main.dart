5b17c7475dbd5ebaf269c24f6e799b2f75d55254
kott <kott2556@gmail.com>
2026-01-14 06:52:18 +0700

    เปลี่ยน ชื่อ หัวเว็บเป็น ลับแล BARBER  14-1-2569 เวลา 6.51 น.

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Barber Queue',
+      title: 'ลับแล BARBER',
       initialRoute: '/',
       routes: {
         // ---------- USER ----------
